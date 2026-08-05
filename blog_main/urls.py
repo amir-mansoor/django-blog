626db79db41a488692ab35c3240f5173bf779d0f
= <qazimansor89@gmail.com>
2026-08-05 10:13:06 +0500

    show posts by category

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.home,name="home"),
     path('admin/', admin.site.urls),
+    path("category/",include('blogs.urls'))
 ]
 
 if settings.DEBUG:
